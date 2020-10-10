938683b4c75d5b1abdaff833d5a316a389cbe622
Moses Akor <gabrielmosesakor@gmail.com>
2020-10-10 21:03:53 +0100

    added a few gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,3 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+Paperclip.options[:command_path] = "/home/linuxbrew/.linuxbrew/bin/"
